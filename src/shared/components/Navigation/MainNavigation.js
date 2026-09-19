019ec6d62d443cab393233c1359686d057137620
trisha217 <trishamoitra.217@gmail.com>
2026-09-19 14:58:20 +0530

    Add SideDrawer and Backdrop components for improved navigation; update MainNavigation to utilize new components and integrate react-transition-group for animations. Update package.json and package-lock.json to include new dependencies.

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainHeader from './MainHeader';
 import {Link} from 'react-router-dom';
 import NavLinks from './NavLinks';
 import './MainNavigation.css'
-
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop/Backdrop.js';
 const MainNavigation = props => {
-    return (<MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span/>
-            <span/>
-            <span/>
-        </button>
-        <h1 className="main-navigation__title"><Link to ="/">YOUR PLACES</Link></h1>
-        <nav><NavLinks/></nav>
-    </MainHeader>
+
+    const [drawerIsOpen,setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () =>
+    {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawer = () =>
+    {
+        setDrawerIsOpen(false);
+    }
+
+    return (
+    <React.Fragment>
+
+        {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+        <nav className="main-navigation__drawer-nav"><NavLinks/></nav>
+        </SideDrawer >
+
+        <MainHeader>
+            <button className="main-navigation__menu-btn" onClick = {openDrawer}>
+                <span/>
+                <span/>
+                <span/>
+            </button>
+            <h1 className="main-navigation__title"><Link to ="/">YOUR PLACES</Link></h1>
+            <nav className="main-navigation__header-nav"><NavLinks/></nav>
+        </MainHeader>
+    </React.Fragment>
     );
 };
 
